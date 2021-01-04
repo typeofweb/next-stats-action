8219e55b8918760aa2515acec6943637c5851cb5
Michal Miszczyszyn <michal@mmiszy.pl>
2021-01-04 02:20:01 +0100

    test

--- src/prepare/repo-setup.js
+++ src/prepare/repo-setup.js
@@ -11,8 +11,8 @@ module.exports = actionInfo => {
       await exec(`git clone ${actionInfo.gitRoot}${repoPath} ${dest}`)
     },
     async checkoutRef(ref = '', repoDir = '') {
-      console.log(await exec(`git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*" --replace-all && git config --get remote.origin.fetch`));
-      console.log(await exec(`cd ${repoDir} && git remote update && git fetch --prune && git checkout ${ref}`));
+      console.log(await exec(`cd ${repoDir} && git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*" --replace-all && git config --get remote.origin.fetch`));
+      console.log(await exec(`git remote update && git fetch --prune && git checkout ${ref}`));
     },
     async getLastStable(repoDir = '', ref) {
       const { stdout } = await exec(`cd ${repoDir} && git tag -l`)
